139f2b9c3a578c50f65b45671085e1e6f56bdea3
AlejandroAL2704 <alejoagudelo12@hotmail.com>
2025-02-23 21:33:41 -0500

    programa basico de python

---
+++ app.py
@@ -0,0 +1,17 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"Hola Mundo!")
+
+def run_server():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print("Iniciando servidor en el puerto 8000...")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run_server()
